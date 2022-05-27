3eb5aca2e3c0b30c7e1fce6e09c6148ba0a1c357
Michaël Roy <microygh@gmail.com>
2022-05-27 14:04:46 +0200

    Séparation en deux fonctions.

--- Beer.py
+++ Beer.py
@@ -8,11 +8,15 @@
 # Dépendance
 import random
 
-# Fonction permettant de lire le fichier de la liste des bières
-# et d'en choisir une au hasard
-def RandomBeer() :
+# Fonction permettant de lire le fichier de la liste des bières et la renvoyer
+def GetBeerList() :
 	# Lit le fichier de la liste des bières
 	with open( 'Beer.txt' ) as beer_file :
 		beer_list = beer_file.read().splitlines()
+	# Renvoie la liste des bières
+	return beer_list
+
+# Fonction permettant de retourner une bière au hasard en fonction de la liste
+def GetRandomBeer( beer_list ) :
 	# Choisit une bière au hasard dans la liste et la renvoie
 	return random.choice( beer_list )
